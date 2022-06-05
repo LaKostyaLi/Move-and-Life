3b5bf828dff27ae5bf38efa427d03a3e0a2bc03d
LakostyaLi <lakostyali1996@gmail.com>
2022-06-05 18:08:09 +0300

    Start new project

---
+++ Move and Life/Assets/Scripts/MovePlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rb;
+    [SerializeField] private FixedJoystick _joystick;
+    [SerializeField] private Animator _animation;
+
+    [SerializeField] private float _moveSpeed;
+
+    private void FixedUpdate()
+    {
+        _rb.velocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rb.velocity.y, _joystick.Vertical * _moveSpeed); 
+    }
+}
