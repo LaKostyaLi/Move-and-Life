aae7d1da7f58b8201e72ab99bc5a6a56e68f1bf1
LakostyaLi <lakostyali1996@gmail.com>
2022-06-11 08:19:42 +0300

    Add sound step №1

---
+++ Move and Life/Assets/Scripts/SoundStep.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundStep : MonoBehaviour
+{
+   [SerializeField] private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void StepSound()
+    {
+        _audioSource.Play();
+    }
+}
