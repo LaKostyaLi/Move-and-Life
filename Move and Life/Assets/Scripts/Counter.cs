375475985752a237afc5ce01e563ac7d19aa88f1
LakostyaLi <lakostyali1996@gmail.com>
2022-06-19 00:32:37 +0300

    add inventory and craft

--- Move and Life/Assets/Scripts/Counter.cs
+++ Move and Life/Assets/Scripts/Counter.cs
@@ -5,18 +5,44 @@ using UnityEngine.UI;
 
 public class Counter : MonoBehaviour
 {
+    //resource
     public int _spichki = 0;
     public int _bumaga = 0;
     public int _shepki = 0;
+    //lvl fire
+    public int _fire_1 = 0;
+    public int _fire_2 = 0;
+    public int _fire_3 = 0;
 
     public Text _spichkiText;
     public Text _bumagaText;
     public Text _shepkiText;
+
+    public Text _fire_1Text;
+    public Text _fire_2Text;
+    public Text _fire_3Text;
 
     public void CounterUpdate() 
     {
         _spichkiText.text = "" + _spichki;
         _bumagaText.text = "" + _bumaga;
         _shepkiText.text = "" + _shepki;
+
+        _fire_1Text.text = "" + _fire_1;
+        _fire_2Text.text = "" + _fire_2;
+        _fire_3Text.text = "" + _fire_3;
+
+        if(_spichki> 2)
+        {
+
+        }
+        if (_bumaga > 2)
+        {
+
+        }
+        if (_shepki > 2)
+        {
+
+        }
     }
 }
--- Move and Life/Assets/Scripts/Craft.cs
+++ Move and Life/Assets/Scripts/Craft.cs
@@ -4,15 +4,154 @@ using UnityEngine;
 
 public class Craft : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int _craftItemNumber=0;
+    public Counter _counter;
+
+    private bool task1_1 = true;
+    private bool task1_2 = true;
+    private bool task1_3 = true;
+
+    private bool task2_1 = true;
+    private bool task2_2 = true;
+
+    private bool task3_1 = true;
+    private bool task3_2 = true;
+
+    public void CraftItemUpdate(int _myNumber)
     {
-        
+        _craftItemNumber = _myNumber;
+        print(_craftItemNumber);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if(Input.GetKeyDown (KeyCode.E))
+        {
+            if(_craftItemNumber ==1)
+            {
+                Fire1();
+            }
+
+            if (_craftItemNumber == 2)
+            {
+                Fire2();
+            }
+
+            if (_craftItemNumber == 3)
+            {
+                Fire3();
+            }
+        }
+    }
+
+    public void Fire1()
+    {
+        if(_counter._spichki < 1)
+        {
+            task1_1 = false;
+            Debug.Log("Не хватает спичек");
+        }
+        else
+        {
+            task1_1 = true;
+        }
+
+        if(_counter._bumaga < 1)
+        {
+            task1_2 = false;
+            Debug.Log("Не хватает бумаги");
+        }
+        else
+        {
+            task1_2 = true;
+        }
+
+        if(_counter._shepki < 1)
+        {
+            task1_3 = false;
+            Debug.Log("Не хватает щепок");
+        }
+        else
+        {
+            task1_3 = true;
+        }
+
+        if (task1_1==true & task1_2 == true & task1_3 ==true)
+        {
+            _counter._spichki -= 1;
+            _counter._bumaga -= 1;
+            _counter._shepki -= 1;
+            _counter._fire_1 += 1;
+            _counter.CounterUpdate();
+            _craftItemNumber = 0;
+        }
+    }
+
+    public void Fire2()
+    {
+        if(_counter._fire_1 < 2)
+        {
+            task2_1 = false;
+            Debug.Log("Не хватает огня уровня 1");
+        }
+
+        else
+        {
+            task2_1 = true;
+        }
+
+
+        if (task2_1 == true)
+        {
+            _counter._fire_1 -= 2;
+            _counter._fire_2 += 1;
+            _counter.CounterUpdate();
+            _craftItemNumber = 0;
+        }
+    }
+
+    public void Fire3()
+    {
+        if (_counter._fire_1 < 1)
+        {
+            Debug.Log("Не хватает огня уровня 1");
+            task3_1 = false;
+        }
+        else
+        {
+            task3_1 = true;
+        }
+
+        if (_counter._fire_2 < 1)
+        {
+            Debug.Log("Не хватает огня уровня 2");
+            task3_2 = false;
+        }
+        else
+        {
+            task3_2 = true;
+        }
+
+        if (task3_1 == true & task3_2 == true)
+        {
+            StartCoroutine(TimeFoFire3());
+        }
+    }   
+
+    IEnumerator TimeFoFire3()
+    {
+        yield return new WaitForSeconds(5f);
+
+        _counter._fire_1 -= 1;
+        _counter._fire_2 -= 1;
+        _counter._fire_3 += 1;
+        _counter.CounterUpdate();
+        _craftItemNumber = 0;
     }
 }
+
+
+
+   
+
+
